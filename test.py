21dbc096aeda85ce5dead77db21d98552302f0de
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-17 22:57:27 +0200

    Ajout de test.

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 # affiche "hello world"
-print("hello world");
+print("hello world")
+
+# variables
+variable_1 = "une variable"
+variable_2 = "une deuxième variable"
+print(variable_1)
+print(variable_2)
