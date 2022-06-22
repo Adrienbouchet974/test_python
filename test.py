ec8faad8599bf6338e3aa81a321bd7477fbdac93
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-22 19:24:57 +0200

    Ajout de test avec une liste et une chaîne de caractères.

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ variable_1 = "une variable"
 variable_2 = "une deuxième variable"
 print(variable_1)
 print(variable_2)
+
+# liste
+plateformes_sociales = ["facebook", "messenger", "twitter", "snapchat", "LinkedIn"]
+langage_de_programmation = "PYTHON"
+# Affiche "twitter"
+print(plateformes_sociales[2])
+# Affiche la lettre "T" qui est l'indice [2] de la chaîne de caractères "PYTHON"
+print(langage_de_programmation[2])
+# Affiche la lettre "i" qui est l'indice [2] de la chaîne de caractères "twitter" contenu dans un array ou twitter est à l'index [2];
+print(plateformes_sociales[2][2])
