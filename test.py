ea4fcee9c6541fdc313725033edd4246def95a51
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-07-28 09:16:12 +0200

    Ajout des tests pour créer un dictionnaire

--- test.py
+++ test.py
@@ -35,3 +35,21 @@ print(plateformes_sociales)
 # S'écrit comme une liste sauf que les crochets [] sont remplacés par des parenthèses ()
 # La principale différence est que les tuples sont immuables ! ( ce sont toujours des listes ! )
 exmples_tuples = (1, 2, 3, 4, 5)
+
+
+# Les dictionnaires ( équivalant aux objets en js )
+nouvelle_campagne = {
+    "responsable_de_campagne": "Jeanne d'Arc",
+    "nom_de_campagne": "Campagne nous aimons les chiens",
+    "date_de_début": "01/01/2020",
+    "influenceurs_importants": ["@MonAmourDeChien", "@MeilleuresFriandisesPourChiens"]
+}
+# Ou bien
+taux_de_conversion = {}
+taux_de_conversion['facebook'] = 3.4
+taux_de_conversion['instagram'] = 1.2
+# Ou encore en utilisant la fonction dict()
+# taux_de_conversion = dict()
+# taux_de_conversion['facebook'] = 3.4
+# taux_de_conversion['instagram'] = 1.2
+print(taux_de_conversion)
